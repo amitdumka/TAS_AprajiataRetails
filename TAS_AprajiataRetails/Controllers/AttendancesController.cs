9267e04b557d91d9480021dc8803ab9d4dad2a15
Amit Kumar <amitnarayansah@gmail.com>
2019-12-30 16:00:52 +0530

    att list activated

--- TAS_AprajiataRetails/Controllers/AttendancesController.cs
+++ TAS_AprajiataRetails/Controllers/AttendancesController.cs
@@ -13,35 +13,107 @@ namespace TAS_AprajiataRetails.Controllers
     [Authorize]
     public class AttendancesController : Controller
     {
-        private AprajitaRetailsContext db = new AprajitaRetailsContext();
+
+        static int CountDays(DayOfWeek day, DateTime start, DateTime end)
+        {
+            TimeSpan ts = end - start;                       // Total duration
+            int count = (int) Math.Floor (ts.TotalDays / 7);   // Number of whole weeks
+            int remainder = (int) ( ts.TotalDays % 7 );         // Number of remaining days
+            int sinceLastDay = (int) ( end.DayOfWeek - day );   // Number of days since last [day]
+            if ( sinceLastDay < 0 )
+                sinceLastDay += 7;         // Adjust for negative days since last [day]
+
+            // If the days in excess of an even week are greater than or equal to the number days since the last [day], then count this one, too.
+            if ( remainder >= sinceLastDay )
+                count++;
+
+            return count;
+        }
+        static int CountDays(DayOfWeek day, DateTime curMnt)
+        {
+            DateTime start = new DateTime (curMnt.Year, curMnt.Month, 1);
+            DateTime end = new DateTime (curMnt.Year, curMnt.Month, DateTime.DaysInMonth (curMnt.Year, curMnt.Month));
+            TimeSpan ts = end - start;                       // Total duration
+            int count = (int) Math.Floor (ts.TotalDays / 7);   // Number of whole weeks
+            int remainder = (int) ( ts.TotalDays % 7 );         // Number of remaining days
+            int sinceLastDay = (int) ( end.DayOfWeek - day );   // Number of days since last [day]
+            if ( sinceLastDay < 0 )
+                sinceLastDay += 7;         // Adjust for negative days since last [day]
+
+            // If the days in excess of an even week are greater than or equal to the number days since the last [day], then count this one, too.
+            if ( remainder >= sinceLastDay )
+                count++;
+
+            return count;
+        }
+
+        private AprajitaRetailsContext db = new AprajitaRetailsContext ();
 
         // GET: Attendances
         public ActionResult Index()
         {
-            var attendances = db.Attendances.Include(a => a.Employee).Where(c=>DbFunctions.TruncateTime(c.AttDate)==DbFunctions.TruncateTime(DateTime.Today)).OrderByDescending(c=>c.AttDate);
-            return View(attendances.ToList());
+            var attendances = db.Attendances.Include (a => a.Employee).Where (c => DbFunctions.TruncateTime (c.AttDate) == DbFunctions.TruncateTime (DateTime.Today)).OrderByDescending (c => c.AttDate);
+            return View (attendances.ToList ());
+        }
+
+        public ActionResult EmpDetails(int? id)
+        {
+            if ( id == null )
+            {
+                return new HttpStatusCodeResult (HttpStatusCode.BadRequest);
+            }
+            //Attendance attendance = db.Attendances.Include (c => c.Employee).Where (c => c.AttendanceId == id).FirstOrDefault ();
+
+            var empid = db.Attendances.Find (id).EmployeeId;
+            var attList = db.Attendances.Include (c => c.Employee)
+                .Where (c => c.EmployeeId == empid && DbFunctions.TruncateTime (c.AttDate).Value.Month == DbFunctions.TruncateTime (DateTime.Today).Value.Month)
+                .OrderBy (c => c.AttDate);
+
+            var p = attList.Where (c => c.Status == AttUnits.Present).Count ();
+            var a = attList.Where (c => c.Status == AttUnits.Absent).Count ();
+            int noofdays = DateTime.DaysInMonth (DateTime.Today.Year, DateTime.Today.Month);
+            int noofsunday = CountDays (DayOfWeek.Sunday, DateTime.Today);
+            int sunPresent = attList.Where (c => c.Status == AttUnits.Sunday).Count ();
+            int halfDays= attList.Where (c => c.Status == AttUnits.HalfDay).Count ();
+            int totalAtt = p + sunPresent + ( halfDays / 2 );
+
+            ViewBag.Present = p;
+            ViewBag.Absent = a;
+            ViewBag.WorkingDays = noofdays;
+            ViewBag.Sundays = noofsunday;
+            ViewBag.SundayPresent = sunPresent;
+            ViewBag.HalfDays = halfDays;
+            ViewBag.Total = totalAtt;
+
+
+            if ( attList == null )
+            {
+                return HttpNotFound ();
+            }
+            ViewBag.EmpName = attList.First ().Employee.StaffName;
+            return PartialView (attList);
         }
 
         // GET: Attendances/Details/5
         public ActionResult Details(int? id)
         {
-            if (id == null)
+            if ( id == null )
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return new HttpStatusCodeResult (HttpStatusCode.BadRequest);
             }
-            Attendance attendance = db.Attendances.Find(id);
-            if (attendance == null)
+            Attendance attendance = db.Attendances.Find (id);
+            if ( attendance == null )
             {
-                return HttpNotFound();
+                return HttpNotFound ();
             }
-            return PartialView(attendance);
+            return PartialView (attendance);
         }
 
         // GET: Attendances/Create
         public ActionResult Create()
         {
-            ViewBag.EmployeeId = new SelectList(db.Employees, "EmployeeId", "StaffName");
-            return PartialView();
+            ViewBag.EmployeeId = new SelectList (db.Employees, "EmployeeId", "StaffName");
+            return PartialView ();
         }
 
         // POST: Attendances/Create
@@ -49,33 +121,33 @@ namespace TAS_AprajiataRetails.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "AttendanceId,EmployeeId,AttDate,EntryTime,Status,Remarks")] Attendance attendance)
+        public ActionResult Create([Bind (Include = "AttendanceId,EmployeeId,AttDate,EntryTime,Status,Remarks")] Attendance attendance)
         {
-            if (ModelState.IsValid)
+            if ( ModelState.IsValid )
             {
-                db.Attendances.Add(attendance);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                db.Attendances.Add (attendance);
+                db.SaveChanges ();
+                return RedirectToAction ("Index");
             }
 
-            ViewBag.EmployeeId = new SelectList(db.Employees, "EmployeeId", "StaffName", attendance.EmployeeId);
-            return View(attendance);
+            ViewBag.EmployeeId = new SelectList (db.Employees, "EmployeeId", "StaffName", attendance.EmployeeId);
+            return View (attendance);
         }
 
         // GET: Attendances/Edit/5
         public ActionResult Edit(int? id)
         {
-            if (id == null)
+            if ( id == null )
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return new HttpStatusCodeResult (HttpStatusCode.BadRequest);
             }
-            Attendance attendance = db.Attendances.Find(id);
-            if (attendance == null)
+            Attendance attendance = db.Attendances.Find (id);
+            if ( attendance == null )
             {
-                return HttpNotFound();
+                return HttpNotFound ();
             }
-            ViewBag.EmployeeId = new SelectList(db.Employees, "EmployeeId", "StaffName", attendance.EmployeeId);
-            return PartialView(attendance);
+            ViewBag.EmployeeId = new SelectList (db.Employees, "EmployeeId", "StaffName", attendance.EmployeeId);
+            return PartialView (attendance);
         }
 
         // POST: Attendances/Edit/5
@@ -83,51 +155,51 @@ namespace TAS_AprajiataRetails.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "AttendanceId,EmployeeId,AttDate,EntryTime,Status,Remarks")] Attendance attendance)
+        public ActionResult Edit([Bind (Include = "AttendanceId,EmployeeId,AttDate,EntryTime,Status,Remarks")] Attendance attendance)
         {
-            if (ModelState.IsValid)
+            if ( ModelState.IsValid )
             {
-                db.Entry(attendance).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                db.Entry (attendance).State = EntityState.Modified;
+                db.SaveChanges ();
+                return RedirectToAction ("Index");
             }
-            ViewBag.EmployeeId = new SelectList(db.Employees, "EmployeeId", "StaffName", attendance.EmployeeId);
-            return View(attendance);
+            ViewBag.EmployeeId = new SelectList (db.Employees, "EmployeeId", "StaffName", attendance.EmployeeId);
+            return View (attendance);
         }
 
         // GET: Attendances/Delete/5
         public ActionResult Delete(int? id)
         {
-            if (id == null)
+            if ( id == null )
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return new HttpStatusCodeResult (HttpStatusCode.BadRequest);
             }
-            Attendance attendance = db.Attendances.Find(id);
-            if (attendance == null)
+            Attendance attendance = db.Attendances.Find (id);
+            if ( attendance == null )
             {
-                return HttpNotFound();
+                return HttpNotFound ();
             }
-            return PartialView(attendance);
+            return PartialView (attendance);
         }
 
         // POST: Attendances/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost, ActionName ("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Attendance attendance = db.Attendances.Find(id);
-            db.Attendances.Remove(attendance);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            Attendance attendance = db.Attendances.Find (id);
+            db.Attendances.Remove (attendance);
+            db.SaveChanges ();
+            return RedirectToAction ("Index");
         }
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing)
+            if ( disposing )
             {
-                db.Dispose();
+                db.Dispose ();
             }
-            base.Dispose(disposing);
+            base.Dispose (disposing);
         }
     }
 }
