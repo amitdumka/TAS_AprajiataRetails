75a5d15787c1d4317b0e37d23b8adbd84ba82390
Amit Kumar <amitnarayansah@gmail.com>
2020-01-10 02:18:43 +0530

    basic

--- TAS_AprajiataRetails/Models/Data/AprajitaRetailsContext.cs
+++ TAS_AprajiataRetails/Models/Data/AprajitaRetailsContext.cs
@@ -12,55 +12,55 @@ namespace TAS_AprajiataRetails.Models.Data
         }
 
 
-        ////Version 2
-        public DbSet<DailySale> DailySales { get; set; }
-        public DbSet<CashInHand> CashInHands { get; set; }
-        public DbSet<CashInBank> CashInBanks { get; set; }
-        public DbSet<Salesman> Salesmen { get; set; }
-        public DbSet<DuesList> DuesLists { get; set; }
+        //Version 2
+        public DbSet<DailySale> DailySales { get; set; } //OK
+        public DbSet<CashInHand> CashInHands { get; set; } //OK
+        public DbSet<CashInBank> CashInBanks { get; set; } //OK
+        public DbSet<Salesman> Salesmen { get; set; } //OK
+        public DbSet<DuesList> DuesLists { get; set; } //OK
 
-        public DbSet<TranscationMode> TranscationModes { get; set; }
-        public DbSet<CashPayment> CashPayments { get; set; }
-        public DbSet<CashReceipt> CashReceipts { get; set; }
+        public DbSet<TranscationMode> TranscationModes { get; set; } //OK
+        public DbSet<CashPayment> CashPayments { get; set; } //OK
+        public DbSet<CashReceipt> CashReceipts { get; set; } //OK
 
         //Payroll
-        public DbSet<Attendance> Attendances { get; set; }
-        public DbSet<Employee> Employees { get; set; }
-        public DbSet<SalaryPayment> Salaries { get; set; }
-        public DbSet<StaffAdvancePayment> StaffAdvancePayments { get; set; }
-        public DbSet<StaffAdvanceReceipt> StaffAdvanceReceipts { get; set; }
+        public DbSet<Attendance> Attendances { get; set; } //OK
+        public DbSet<Employee> Employees { get; set; } //OK
+        public DbSet<SalaryPayment> Salaries { get; set; }//SalaryPayments
+        public DbSet<StaffAdvancePayment> StaffAdvancePayments { get; set; } //OK
+        public DbSet<StaffAdvanceReceipt> StaffAdvanceReceipts { get; set; } //OK
 
-        public DbSet<CurrentSalary> CurrentSalaries { get; set; }
-        public DbSet<PaySlip> PaySlips { get; set; }
+        public DbSet<CurrentSalary> CurrentSalaries { get; set; } //OK
+        public DbSet<PaySlip> PaySlips { get; set; } //OK
 
         //Banking
-        public DbSet<Bank> Banks { get; set; }
-        public DbSet<AccountNumber> BankAccounts { get; set; }
-        public DbSet<BankDeposit> BankDeposits { get; set; }
-        public DbSet<BankWithdrawal> Withdrawals { get; set; }
+        public DbSet<Bank> Banks { get; set; } //OK
+        public DbSet<AccountNumber> BankAccounts { get; set; } //AccountNumbers
+        public DbSet<BankDeposit> BankDeposits { get; set; } //OK
+        public DbSet<BankWithdrawal> Withdrawals { get; set; }//BankWdithdrawals
 
              
 
         //Expenses
-        public DbSet<Expense> Expenses { get; set; }
-        public DbSet<PettyCashExpense> CashExpenses { get; set; }
-        public DbSet<Payment> Payments { get; set; }
-        public DbSet<Receipt> Receipts { get; set; }
+        public DbSet<Expense> Expenses { get; set; } //OK
+        public DbSet<PettyCashExpense> CashExpenses { get; set; } //PettyCashExpenses   
+        public DbSet<Payment> Payments { get; set; } //OK
+        public DbSet<Receipt> Receipts { get; set; } //OK
 
         //Suspense
-        public DbSet<SuspenseAccount> Suspenses { get; set; }
+        public DbSet<SuspenseAccount> Suspenses { get; set; }// SuspensesAccounts
 
 
         //Tailoring
 
-        public DbSet<TailorAttendance> TailorAttendances { get; set; }
-        public DbSet<TailoringEmployee> Tailors { get; set; }
-        public DbSet<TailoringSalaryPayment> TailoringSalaries { get; set; }
-        public DbSet<TailoringStaffAdvancePayment> TailoringStaffAdvancePayments { get; set; }
-        public DbSet<TailoringStaffAdvanceReceipt> TailoringStaffAdvanceReceipts { get; set; }
+        public DbSet<TailorAttendance> TailorAttendances { get; set; } //OK
+        public DbSet<TailoringEmployee> Tailors { get; set; }//TailoringEmployees
+        public DbSet<TailoringSalaryPayment> TailoringSalaries { get; set; }//TailoringSalaryPayments
+        public DbSet<TailoringStaffAdvancePayment> TailoringStaffAdvancePayments { get; set; } //OK
+        public DbSet<TailoringStaffAdvanceReceipt> TailoringStaffAdvanceReceipts { get; set; } //OK
 
-        public DbSet<TalioringBooking> Bookings { get; set; }
-        public DbSet<TalioringDelivery> Deliveries { get; set; }
+        public DbSet<TalioringBooking> Bookings { get; set; }//TailoringBookings
+        public DbSet<TalioringDelivery> Deliveries { get; set; }//TailoringDeliveries
 
 
         //End of Day
@@ -69,9 +69,9 @@ namespace TAS_AprajiataRetails.Models.Data
 
         //Others
 
-        public DbSet<DueRecoverd>Recoverds { get; set; }
-        public DbSet<ChequesLog>Cheques { get; set; }
+        public DbSet<DueRecoverd>Recoverds { get; set; }//DuesRecovereds
+        public DbSet<ChequesLog>Cheques { get; set; }//ChequesLogs
 
-        public DbSet<MonthEnd> MonthEnds { get; set; }
+        public DbSet<MonthEnd> MonthEnds { get; set; } //OK
     }
 }
